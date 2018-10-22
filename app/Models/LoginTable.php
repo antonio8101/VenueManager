103e8ce2b43fcdc6b89ccb24becf4ca473d01bea
Antonio <antonio8101@gmail.com>
2018-10-22 02:53:12 +0200

    refactoring

--- app/Models/LoginTable.php
+++ app/Models/LoginTable.php
@@ -227,20 +227,3 @@ class SessionExpiredException extends Exception {
 	}
 
 }
-
-
-class AuthenticationException extends Exception {
-
-	public function __construct( string $message = "", int $code = 0, Throwable $previous = null ) {
-		parent::__construct( $message, $code, $previous );
-	}
-
-	public function __toString() {
-		return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
-	}
-
-	public function customFunction() {
-		echo "A custom function for this type of exception\n";
-	}
-
-}
