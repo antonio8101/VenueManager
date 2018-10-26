9b90948b224e81d649193e9f10208e163a66bfd5
Antonio <antonio8101@gmail.com>
2018-10-27 00:05:44 +0200

    route restyling

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ Route::any( '/api/login', 'AuthController@login' )->name( 'loginAttempt' );
 
 /** API USER */
 Route::get( '/api/user/get/{id}', 'Api\UserController@getOneUserQuery' )->name('GetOneUserQuery');
-Route::any( '/api/user/list', 'Api\UserController@getUsersQuery' )->name('UsersQuery');
+Route::any( '/api/user/search', 'Api\UserController@getUsersQuery' )->name('UsersQuery');
 Route::any( '/api/user/create', 'Api\UserController@createUserCommand' )->name('CreateUserCommand');
 Route::any( '/api/user/edit', 'Api\UserController@editUserCommand' )->name('EditUserCommand');
 
 /** API VENUE */
 Route::get( '/api/venue/get/{id}', 'Api\VenueController@getOneVenueQuery' )->name('GetOneVenueQuery');
-Route::any( '/api/venue/list', 'Api\VenueController@getVenuesQuery' )->name('VenuesQuery');
+Route::any( '/api/venue/search', 'Api\VenueController@getVenuesQuery' )->name('VenuesQuery');
 Route::any( '/api/venue/create', 'Api\VenueController@createVenueCommand' )->name('CreateVenueCommand');
 Route::any( '/api/venue/edit', 'Api\VenueController@editVenueCommand' )->name('EditVenueCommand');
